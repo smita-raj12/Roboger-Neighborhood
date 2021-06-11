9193923eedb1339b7ca1729c58e7e40dbca18979
smita Raj <smita.raj12@gmail.com>
2021-06-11 16:05:49 -0700

    Added name field

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 
-function beepBoop(i){
+function beepBoop(i,username){
   let strNum= i.toString();
   let strNumArray=strNum.split("");
   let outputStr = " "
     
   if(strNumArray.includes("3")){
-      return outputStr = `"Wont you be my neighbor?"` 
+      return outputStr = `"Wont you be my neighbor? ${username} "` 
   }
   else if(strNumArray.includes("2")){
     return  outputStr = `"Boop!"`
@@ -18,20 +18,20 @@ function beepBoop(i){
   }
 }
 
-function ForwardLoop(userNum){
+function ForwardLoop(userNum,username){
   let outputStrArray = [];
 
   for ( i = 0; i <= userNum ; i++){
-    let outputStr = beepBoop(i,outputStrArray)
+    let outputStr = beepBoop(i,username)
     outputStrArray.push(outputStr)
   }
   return outputStrArray.join(  "  ,  "  )
 }
 
-function ReverseLoop(userNum){
+function ReverseLoop(userNum,username){
   let outputStrArray = [];
   for (let i = userNum; i >= 0; i--){
-    let outputStr=beepBoop(i,outputStrArray)
+    let outputStr=beepBoop(i,username)
     outputStrArray.push(outputStr)
   }  
   return outputStrArray.join(  "  ,  "  )
@@ -41,8 +41,9 @@ $(document).ready(function() {
   $("#formRoger").submit(function(event){
     event.preventDefault();
     const UserNum = $("input#num").val();
-    const BeepBoop = ForwardLoop(UserNum)
-    const ReverseBeepBoop = ReverseLoop(UserNum)
+    const UserName = $("input#username").val();
+    const BeepBoop = ForwardLoop(UserNum,UserName)
+    const ReverseBeepBoop = ReverseLoop(UserNum,UserName)
     $("#output").html(BeepBoop);
     $("#Routput").html(ReverseBeepBoop);
   });
