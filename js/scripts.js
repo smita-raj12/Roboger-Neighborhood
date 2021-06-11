295acfa558693f1ef9499f10eccb7eeefcf52a91
smita Raj <smita.raj12@gmail.com>
2021-06-11 09:50:56 -0700

    Bug Fix: trying to fix bug converting uotputstr to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,11 @@
-function beepBoop(Num){
+function beepBoop(userNum){
   let outputStrArray = [];
-  for (let i = 0; i <= Num ; i++){
+  for (let i = 0; i <= userNum ; i++){
     let strNum= i.toString();
     let strNumArray=strNum.split("");
     console.log(strNumArray)
     let outputStr = " "
     
-    /*switch (strNumArray){
-      case strNumArray.includes("3"):{
-          outputStr = "Won't you be my neighbor?" 
-      }
-      case strNumArray.includes("2"):{
-        outputStr = "Boop!" 
-      }
-      case strNumArray.includes("1"):{
-        outputStr = "Beep" 
-      }
-      default :{
-        outputStr = strNumArray;
-      }
-    }/* end switch*/
     if(strNumArray.includes("3")){
       outputStr = "Wont you be my neighbor" 
     }
@@ -33,8 +19,7 @@ function beepBoop(Num){
       outputStr = strNumArray.toString();
     }
     outputStrArray.push(outputStr)
-      console.log("outputStr",outputStr)
   }
-  console.log("outputStrArray",outputStrArray,Num)
+  
 }
 
